c4cac5862acc58f0b503d2d8713d931e1a859cc2
“Enter <shaikhfaisal6342@gmail.com>
2020-10-17 11:04:09 +0530

    Welcom Message

---
+++ EmpWage.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empwage;
+
+/**
+ *
+ * @author Woner
+ */
+    interface wage{
+        void calEmpWage();
+    }
+
+public class EmpWage implements wage {
+    
+    public void calEmpWage(){
+        System.out.println("Welcome to Employee Wage");
+    }
+    
+    public static void main(String[] args) {
+            
+        EmpWage w=new EmpWage();
+        
+        w.calEmpWage();
+        
+        }
+    }
+
