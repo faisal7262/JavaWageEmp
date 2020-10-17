e21214297982ada5b41b17064f58ef8e05b391c5
“Enter <shaikhfaisal6342@gmail.com>
2020-10-17 11:42:44 +0530

    Daily Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -17,10 +17,16 @@ public class EmpWage implements wage {
     
     public void calEmpWage(){
         int IS_FULL_TIME=1;
+        int WAGE_PER_HOUR=20;
+        int FULL_DAY_HOUR=8;
+        int payment;
         double empCheck = Math.floor(Math.random()*10) % 2;
         System.out.println("Welcome to Employee Wage");
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is present");
+            payment = WAGE_PER_HOUR*FULL_DAY_HOUR;
+            System.out.println("Daily Employee payment :" + payment);
+            
         }
         else
             System.out.println("Employee is Absent");
