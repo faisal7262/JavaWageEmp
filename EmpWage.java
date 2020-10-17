d061a1a3d017e8a1b963e4b74cda854bb6c91976
“Enter <shaikhfaisal6342@gmail.com>
2020-10-17 12:45:24 +0530

    Wages for a Month

--- EmpWage.java
+++ EmpWage.java
@@ -5,6 +5,8 @@
  */
 package empwage;
 
+import java.util.Random;
+
 /**
  *
  * @author Woner
@@ -17,27 +19,37 @@ public class EmpWage implements wage {
     
     public void calEmpWage(){
         int IS_FULL_TIME=1;
-        int IS_PART_TIME=0;
+        int IS_PART_TIME=2;
         int WAGE_PER_HOUR=20;
-        int FULL_DAY_HOUR=8;
-        int PART_TIME_HOUR=4;
-        int payment;
-        double empCheck = Math.floor(Math.random()*10) % 2;
+        int EMP_HOURS = 0;
+        int TOTAL_SALARY;
+        int DAY_PER_MONTH=20;
+        int TOTAL_EMP_HOUR=0;
         
-        System.out.println("Welcome to Employee Wage"+empCheck);
+       
+        //double empCheck = Math.floor(Math.random()*10) % 2;
+        for(int i=1; i<=DAY_PER_MONTH; i++){
+             Random r=new Random();
+            int empCheck = r.nextInt((3-1)+1)+1;
+        
+        System.out.println("Welcome to Employee Wage");
              switch((int)empCheck){
             case 1:
-                payment = WAGE_PER_HOUR*FULL_DAY_HOUR;
-                System.out.println("Full Time Employee payment :" + payment);
+                EMP_HOURS=8;
                 break;
-            case 0:
-                payment = WAGE_PER_HOUR*PART_TIME_HOUR;
-                System.out.println("Part Time Employee payment :" + payment);
+            case 2:
+                EMP_HOURS=4;
                 break;
             default:
                 System.out.println("Employee is Absent");
              }
-            
+             
+             TOTAL_EMP_HOUR=(TOTAL_EMP_HOUR+EMP_HOURS);
+             
+        }
+            TOTAL_SALARY=TOTAL_EMP_HOUR*WAGE_PER_HOUR;
+            System.out.println(TOTAL_SALARY);
+             
     }
     
     public static void main(String[] args) {
